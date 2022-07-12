f6c05efac57e435270dbfff11b681a63b551467c
wonkang2 <wonkang96@gmail.com>
2022-07-13 01:02:39 +0900

    기본 요구 사항 구현

--- kiosk_program/src/Kiosk_Program.java
+++ kiosk_program/src/Kiosk_Program.java
@@ -1,27 +1,37 @@
+
 import java.util.Scanner;
 
 // Kiosk_Program 클래스를 정의 합니다.
 public class Kiosk_Program {
     //TODO:
     // 메뉴 입력 및 사용자의 수량을 입력하기 위해 Scanner 객체 생성
-
+    static Scanner sc = new Scanner(System.in);
     /**
      * @menu_n_cost : 메뉴의 가격을 정의 합니다.
      * 메뉴의 가격은 변하지 않습니다.
      * 해당 기능은 자바독(javadoc) 기능입니다.
      */
     //TODO:
+    static int result = 0;
+    static int menu_1_cost = 1000;
+    static int menu_2_cost = 1500;
+    static int menu_3_cost = 1000;
+    static int menu_4_cost = 2000;
 
     /**
      * 프로그램의 시작을 알리는 메서드입니다.
+     *
      * @.repeat() 메서드는 해당 문자를 반복해서 출력해주는 기능을 갖습니다.
      */
     static void input_print() {
         //TODO:
+        System.out.println("[안내]안녕하세요. 김밥천국에 오신 것을 환영합니다.");
+        System.out.println("-".repeat(25));
     }
 
     /**
      * 사용자가 원하는 메뉴를 번호로 입력받습니다.
+     *
      * @재귀함수 : 본 메서드가 동작할 때 메뉴에 없는 번호가 입력되면 자기 자신이 다시 실행되도록 합니다.
      * 해당 count() 메소드는 기능이 모두 구현되어 있습니다. Scanner 객체의 변수명만 확인해야 합니다.
      */
@@ -31,7 +41,6 @@ public class Kiosk_Program {
         //sc == 최 상단에 입력받은 Scanner 객체
         //변수명이 다르다면 하단의 이름을 수정해야 합니다.
         int number = sc.nextInt();
-
         // 만약 사용자가 입력한 번호의 조건이 아래와 같다면,
         // 0 이하의 조건
         // 4 초과의 조건
@@ -59,20 +68,30 @@ public class Kiosk_Program {
 
     /**
      * 수량 입력 및 가격 연산을 진행하는 메서드를 정의합니다.
+     *
      * @수량 : 수량은 1 ~ 99 까지만 입력 받을 수 있도록 합니다.
      */
     static void cost(int number) {
         // 안내 문구를 출력합니다.
         //TODO:
-
+        System.out.println("-".repeat(25));
+        System.out.println("[안내]선택하신 메뉴의 수량을 입력하여 주세요.");
+        System.out.println("(※ 최대 주문 가능 수량 : 99)");
         // 입력받은 수량을 변수에 저장합니다.
         //TODO:
-
+        int cnt = sc.nextInt();
         // 조건에 따라 문구 출력, 재입력을 진행합니다.
         // 조건은 다음과 같습니다.
         // 0 이하 || 99 초과
         // 두 조건 중 하나라도 참인경우 경고 문구 출력과 함께 다시 수량을 입력받을 수 있도록 합니다.
         //TODO:
+        if (cnt > 99 || cnt < 1) {
+            System.out.println("[경고]" + cnt + "개는 입력하실 수 없습니다.");
+            System.out.println("[경고]수량 선택 화면으로 돌아갑니다.");
+            cost(number);
+            return;  // LWK : 재귀를 호출하고 종료하지 않으면 다시 되돌아오면서 데이터를 수정함.
+        }
+        result = number * cnt;
     }
 
     /**
@@ -80,6 +99,8 @@ public class Kiosk_Program {
      */
     static void output_print(int result) {
         //TODO:
+        System.out.println("[안내]주문하신 메뉴의 총 금액은 " + result + "원 입니다.");
+        System.out.println("[안내]이용해 주셔서 감사합니다.");
     }
 
     /**
@@ -88,5 +109,9 @@ public class Kiosk_Program {
      */
     public static void main(String[] args) {
         //TODO:
+        Kiosk_Program.input_print();
+        Kiosk_Program.cost(Kiosk_Program.count());
+        Kiosk_Program.output_print(Kiosk_Program.result);
+
     }
 }
